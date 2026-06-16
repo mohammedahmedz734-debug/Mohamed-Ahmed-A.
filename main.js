32bca11650334f58c96f08fbdd03fd08d0d3c5c0
Mohamed Ahmed <mohammedahmedz734@gmail.com>
2026-06-17 02:46:29 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+// add class activeto header on scroll
+
+let header = document.querySelector("header")
+
+window.onscroll = function(){
+    if (this.scrollY >=50){
+        header.classList.add("active")
+    }
+    else{
+        header.classList.remove("active")
+    }
+}
+
+
+
+
+let nav_links = document.getElementById("links");
+
+function Open_colose_menu(){
+    nav_links.classList.toggle("active")
+}
